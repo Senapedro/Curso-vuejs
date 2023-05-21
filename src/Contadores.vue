60ae3dde9ab9467eff933e39c3e5d61793963870
pedro sena <ph997595305@gmail.com>
2023-05-20 22:08:10 -0300

    atualização do codigo

---
+++ src/Contadores.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="contadores">
+        <app-contador/>
+    </div>
+</template>
+
+<script>
+import Contador from './Contador.vue'
+export default {
+    name: 'contadores',
+    components: { 'app-contadores': Contador }
+}
+</script>
+
+<style>
+
+</style>
